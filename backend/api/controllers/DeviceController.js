33c66d5880205abedd3ae0f1315cbda618b9de6d
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-04-16 16:32:48 -0400

    created delete and update device services

--- backend/api/controllers/DeviceController.js
+++ backend/api/controllers/DeviceController.js
@@ -31,6 +31,35 @@ class DeviceController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async updateDevice(req, res) {
+    const { id } = req.params;
+    const newInfoDevice = req.body;
+
+    try {
+      await database.Devices.update(newInfoDevice, {
+        where: { id: Number(id) },
+      });
+      const updatedDevice = await database.Devices.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(updatedDevice);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async deleteDevice(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Devices.destroy({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({message:`id ${id} deleted`});
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = DeviceController;
--- backend/api/routes/devicesRoute.js
+++ backend/api/routes/devicesRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get("/devices", DeviceController.getAllDevices);
 router.get("/devices/:id", DeviceController.getDevice);
 router.post("/devices", DeviceController.createDevice);
+router.put("/devices/:id", DeviceController.updateDevice);
+router.delete("/devices/:id", DeviceController.deleteDevice);
 
 module.exports = router;

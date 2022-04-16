7e13aa6f3ed76fa2aceb1b7513718452ab896f03
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-04-16 13:40:11 -0400

    created migration table devices

---
+++ backend/api/migrations/20220416173202-create-devices.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Devices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Color: {
+        type: Sequelize.STRING(16),
+        allowNull: false
+      },
+      PartNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Devices');
+  }
+};
---
+++ backend/api/models/devices.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Devices extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Devices.init({
+    Category: DataTypes.STRING,
+    Color: DataTypes.STRING,
+    PartNumber: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Devices',
+  });
+  return Devices;
+};

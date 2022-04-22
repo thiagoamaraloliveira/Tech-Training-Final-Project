c4adc5af10758f90a1888af5cca43d25018f6734
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-04-22 15:19:30 -0400

    added cors

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -2,7 +2,8 @@ const bodyParser = require("body-parser");
 const devices = require("./devicesRoute");
 const users = require("./usersRoute");
 const categories = require("./categoriesRoute");
+const cors = require("cors");
 
 module.exports = (app) => {
-  app.use(bodyParser.json(), devices, users, categories);
+  app.use(bodyParser.json(),cors(), devices, users, categories);
 };

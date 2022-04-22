458c6cde4dd634d7d16cf24d6ddf849b5e497e46
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-04-22 13:12:07 -0400

    created device create component

---
+++ frontend/src/app/components/device/device-create/device-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-device-create",
+  templateUrl: "./device-create.component.html",
+  styleUrls: ["./device-create.component.css"],
+})
+export class DeviceCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

2a188eeca20ffa18f7b1b5728b2ee2fff2804b3f
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-04-27 20:18:26 -0400

    added create category

--- frontend/src/app/components/category/category-create/category-create.component.ts
+++ frontend/src/app/components/category/category-create/category-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { CategoryService } from "../../../services/category.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-category-create",
@@ -6,7 +8,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./category-create.component.css"],
 })
 export class CategoryCreateComponent implements OnInit {
-  constructor() {}
+  category: any = {
+    name: "",
+  };
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
+  createCategory() {
+    this.categoryService.create(this.category).subscribe(() => {
+      this.categoryService.showMessage("Categoria cadastrada!");
+      this.router.navigateByUrl("/categories");
+    });
+  }
+  cancel() {
+    this.router.navigateByUrl("/categories");
+  }
 }
